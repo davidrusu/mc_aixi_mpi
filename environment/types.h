b3cae6d6e5f9ba1e8d1925847cb7cf578a243726
rmoro <moro1422@mylaurier.ca>
2016-04-16 00:03:52 -0400

    Added support for 128 bit ints

--- environment/types.h
+++ environment/types.h
@@ -1,18 +1,24 @@
 #ifndef INTDEF
+    #include <stdint.h>
     #define INTDEF
-    typedef unsigned long long  u128;
-    typedef unsigned long       u64;
-    typedef unsigned int        u32;
-    typedef unsigned short      u16;
-    typedef unsigned char       u08;
-    typedef signed long long    s128
-    typedef signed long         s64;
-    typedef signed int          s32;
-    typedef signed short        s16;
-    typedef signed char         s08;
+    #ifdef uint128_t
+        typedef uint128_t       u128;
+    #endif
+    typedef uint64_t            u64;
+    typedef uint32_t            u32;
+    typedef uint16_t            u16;
+    typedef uint8_t             u08;
+    
+    #ifdef int128_t
+        typedef int_128_t       s128;
+    #endif
+    typedef int64_t             s64;
+    typedef int32_t             s32;
+    typedef int16_t             s16;
+    typedef int8_t              s08;
 #endif
 
 #ifndef BOOLDEF
     #define BOOLDEF
-    typedef bool*           b_lst;
+    typedef bool *             b_lst;
 #endif
