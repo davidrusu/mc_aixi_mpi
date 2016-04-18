79a680cce47c20deaa7662b35f614686e3ca97ff
David Rusu <davidrusu.me@gmail.com>
2016-04-18 00:10:58 -0400

    Read data from file

--- predict/main.c
+++ predict/main.c
@@ -4,6 +4,34 @@
 #include "../bit_vector.h"
 #include "context_tree.h"
 
+void load_from_file(ContextTree *tree) {
+  char file_name[1000];
+  printf("Enter a file name: ");
+  fgets(file_name, sizeof(file_name), stdin);
+  file_name[strlen(file_name)-1] = 0;
+  FILE *fp = fopen(file_name, "r");
+  if (fp == NULL) {
+    printf("File not found '%s'\n", file_name);
+    return;
+  }
+  char line[1000];
+  uint64_t line_num = 0;
+  while (fgets(line, sizeof(line), fp) != NULL) {
+    line_num += 1;
+    if (line_num % 100 == 0) {
+      printf("\rprocessing line %llu\t", line_num);
+    }
+    int32_t i;
+    for (i = 0; i < strlen(line); i++) {
+      char c = line[i];
+      BitVector *v = bv_from_char(c);
+      ctw_update_vector(tree, v);
+      bv_free(v);
+    }
+  }
+  fclose(fp);
+}
+
 int main(int argc, char **argv) {
   printf("repl for testing CTW\n");
   printf("Size of ct_node %lu\n", sizeof(ContextTreeNode));
@@ -26,6 +54,9 @@ int main(int argc, char **argv) {
     } else if (strcmp(pattern, ":l") == 0) {
       ctw_load(tree, "./tree.dat");
       //ctw_node_print(tree->root);
+      continue;
+    } else if (strcmp(pattern, ":f") == 0) {
+      load_from_file(tree);
       continue;
     }
     int32_t i;
