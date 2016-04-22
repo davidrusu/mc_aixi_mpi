8fc63c749098dfebf6dc0da10255276c35cd4a36
rmoro <moro1422@mylaurier.ca>
2016-04-22 19:18:48 -0400

    FIXED 1 ERROR

--- environment/class.h
+++ environment/class.h
@@ -3,13 +3,13 @@
 // EMAIL:    robert@morouney.com
 // FILE:     class.h
 // CREATED:  2016-04-21 12:03:42
-// MODIFIED: 2016-04-21 12:03:42
+// MODIFIED: 2016-04-22 19:18:24
 ////////////////////////////////////////////////////////////////////
 
 #ifndef NEW_H
 #define NEW_H
 
-void * new      ( const void * class, ... );
+void * new      ( void * _class, ... );
 void delete     ( void * item );
 void * cpy        ( const void * self );
 char * print    ( const void * self );
