991e6549ec5a0ee6508b1e255da7a8a493201bf5
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-04-22 19:13:27 -0400

    resolve one compile error of many

--- environment/coin_flip.h
+++ environment/coin_flip.h
@@ -8,6 +8,9 @@
 
 #ifndef COIN_FLIP_H
     #define COIN_FLIP_H
+    #define HEADS_VAL 0x00000001
+    #define TAILS_VAL 0x00000000
+
     #include <stddef.h>
 
     extern const void * Coin_Flip; 
@@ -18,10 +21,10 @@
 
     //GAME CONFIG VARS///////////////////////////////////////////////////
     const double  Default_Probability = 7e-1;
-    const u32     Tails               = 0x00000000;
-    const u32     Heads               = 0x00000001;
-    const u32     Loss                = Tails;
-    const u32     Win                 = Heads;
+    const u32     Tails               = HEADS_VAL;
+    const u32     Heads               = TAILS_VAL;
+    const u32     Loss                = TAILS_VAL;
+    const u32     Win                 = HEADS_VAL;
     const u08     Number_States       = 0x00000002;
 
     double __rp();
