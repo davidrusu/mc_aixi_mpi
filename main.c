3129c04089f46080b3689b76b548b0696317cb08
rmoro <moro1422@mylaurier.ca>
2016-04-22 23:19:29 -0400

    spaced out the output more

--- main.c
+++ main.c
@@ -66,7 +66,6 @@ void _interaction_loop(Agent* agent, struct Environment* environment, app_option
     bool isEnvironmentFinished = false;
     int cycle = 1;
 
-    printf("%-12s%-12s%-12s%-12s%-12s%-12s%-12s%-12s%-12s%-12s\n", "Cycle", "Observation", "Reward", "Action", "Explored", "Explore Rate", "Total Reward", "Average Reward", "Time", "Model Size");
 
     while(!isEnvironmentFinished) {
         int agent_age = agent->age;
@@ -113,7 +112,8 @@ void _interaction_loop(Agent* agent, struct Environment* environment, app_option
         long ticks_taken = time(NULL) - cycle_start;
 
         // Just a large padded statement about what is going on in the world as we step through
-        printf("%-12d%-12u%-12u%-12u%-12d%-12f%-12s%-12s%-12lu%-12s\n", cycle, observation, reward, action, explored, options->exploration, "N/A", "N/A", ticks_taken, "N/A");
+        printf("%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Cycle", "Observation", "Reward", "Action", "Explored", "Explore Rate", "Total Reward", "Average Reward", "Time", "Model Size");
+        printf("%-15d%-15u%-15u%-15u%-15d%-15f%-15s%-15s%-15lu%-15s\n", cycle, observation, reward, action, explored, options->exploration, "N/A", "N/A", ticks_taken, "N/A");
 
         if(explore) {
             options->exploration *= options->explore_decay;
